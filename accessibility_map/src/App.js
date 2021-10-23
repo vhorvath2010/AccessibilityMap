29dbc7abe764466ea56f5a46ea5bd286de2df387
Ethan Kang <EKang@lightboxre.com>
2021-10-23 09:52:18 -0400

    initial create-react-app build, override firebase build

--- accessibility_map/src/App.js
+++ accessibility_map/src/App.js
@@ -1,38 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-import firebase from 'firebase/app';
-import { collection, doc, setDoc, getDoc } from 'firebase/firestore';
-
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-firebase.initializeApp({
-  apiKey: "AIzaSyDS1qBUb0lD0Oh_c5S7r9H0lovndr_MQ3Q",
-  authDomain: "accessibilitymap-7e71b.firebaseapp.com",
-  projectId: "accessibilitymap-7e71b",
-  storageBucket: "accessibilitymap-7e71b.appspot.com",
-  messagingSenderId: "281075792467",
-  appId: "1:281075792467:web:2229748e431e16fceda4a6",
-  measurementId: "G-39QRL73LZF"
-})
-
-const firestore = firebase.fiestore()
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
-}
-
-let id = ""
-const docRef = doc(db, "vendors", id);
-const docSnap = await getDoc(docRef);
-function getStore() {
-  
 }
 
 export default App;
