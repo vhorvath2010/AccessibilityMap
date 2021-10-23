68482717f21b47fd9969e146255bbee14032d35a
Ethan Kang <EKang@lightboxre.com>
2021-10-23 10:35:55 -0400

    initial routing

--- accessibility_map/src/App.js
+++ accessibility_map/src/App.js
@@ -1,25 +1,45 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
+import resultsPage from './pages/resultsPage';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/resultsPage">
+          <resultsPage />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
---
+++ accessibility_map/src/pages/resultsPage.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
+class resultsPage extends React.Component {
+  render(){
+    return (
+        <h2>Home</h2>
+        // <Button variant="contained">Hello World</Button>
+    );
+  }
+}
+
+export default resultsPage;
