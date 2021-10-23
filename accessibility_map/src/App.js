9a27ca411ff410e558b9544bef94ef1a6fc348da
Pei Yi Chiang <USER@lawn-128-61-68-189.lawn.gatech.edu>
2021-10-23 10:34:26 -0400

    firebase set up

--- accessibility_map/src/App.js
+++ accessibility_map/src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import firebase from './firebase';
 
 function App() {
+  const [vendors, setVendors] = useState([]);
+
+  const ref = firebase.firestore().collection('vendors');
+  console.log(ref);
+
+  function getVendors() {
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach(element => {
+        items.push(element.data());
+      });
+      setVendors(items);
+    })
+  }
+  useEffect(() => {
+    getVendors();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Vendors</h1>
+        {vendors.map((vendor) => (
+          <div key={vendor.id}>
+            <h2>{vendor.name}</h2>
+          </div>
+        ))}
       </header>
     </div>
   );
