95352f7292614628cd446005b044353a5be283e9
Vincent Horvath <vhorvath2010@gmail.com>
2021-10-23 16:47:53 -0400

    getVendors change

--- accessibility_map/src/services/getVendors.js
+++ accessibility_map/src/services/getVendors.js
@@ -8,22 +8,27 @@ const db = firebase.firestore().collection('vendors');
 */
 export function getVendors(center, radius) {
     const names = ['restaurant', 'park', 'public transport', 'hair dresser'];
-    var dict = {'restaurant': [], 'park': [], 'public transport': [], 'hair dresser': []};
+    var dict = { 'restaurant': [], 'park': [], 'public transport': [], 'hair dresser': [] };
     if (!center) {
         return dict;
     }
-    db.get().then(vendorsSnapshot => {
-        const vendors = vendorsSnapshot.docs.map(doc => doc.data());
+    var vendors;
+    return db.get().then(vendorsSnapshot => {
+        vendors = vendorsSnapshot.docs.map(doc => doc.data());
+    }, error => {
+        console.log(error);
+    }).then(() => {
         vendors.forEach(vendor => {
             const loc = vendor['latlng'];
             // Convert to geoloc
-            const centerLoc = { latitude: center['lat'], longitude: center['lng']}
-            const geoLoc = { latitude: loc['_lat'], longitude: loc['_long']};
+            const centerLoc = { latitude: center['lat'], longitude: center['lng'] }
+            const geoLoc = { latitude: loc['_lat'], longitude: loc['_long'] };
             // Add vendor if it's within range
             if (isPointWithinRadius(geoLoc, centerLoc, radius)) {
                 dict[names[vendor['type']]].push(vendor);
             }
         });
+    }).then(() => {
+        return dict;
     });
-    return dict;
 };
