643bea0d7d50ee5d37ae2245c7313f13852e3c20
Pei Yi Chiang <USER@lawn-128-61-68-189.lawn.gatech.edu>
2021-10-23 19:33:22 -0400

    connect form to add vendor API

--- accessibility_map/src/services/handleNewVendor.js
+++ accessibility_map/src/services/handleNewVendor.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 
 const db = firebase.firestore().collection('vendors');
 
-export function handleNewVendor(inputs) {
-    const api = "https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(inputs.addr.trim()) + "&key=AIzaSyA2ZpxnUNAG5JdjNi-jv7Ua3r8krKNjbgc";
+export function handleNewVendor(name, addr, busType, serviceRes) {
+    const api = "https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(addr.trim()) + "&key=AIzaSyA2ZpxnUNAG5JdjNi-jv7Ua3r8krKNjbgc";
 
     axios.get(api).then(res => {
         console.log("res = " + JSON.stringify(res.data.results[0].geometry.location.lat))
@@ -14,22 +14,22 @@ export function handleNewVendor(inputs) {
         console.log("lat 1 =" + lat);
 
         console.log("lat 2 =" + lat);
-        console.log("addr = " + inputs.addr);
+        console.log("addr = " + addr);
 
         const latlng = new GeoPoint(lat, lng);
         console.log("Geopoint: " + latlng);
 
         db.doc().set({
-            addr: inputs.addr,
-            animal: inputs.animal,
-            asl: inputs.asl,
-            braille: inputs.braille,
-            curb: inputs.curb,
+            addr: addr,
+            animal: serviceRes['Allow Service Animal'],
+            asl: serviceRes['ASL accommodations'],
+            braille: serviceRes['Braille'],
+            curb: serviceRes['Curbside Service'],
             latlng: latlng,
-            mob: inputs.mob,
-            name: inputs.name,
-            park: inputs.park,
-            type: inputs.type
+            mob: serviceRes['Mobility Access'],
+            name: name,
+            park: serviceRes['Accessible Parking'],
+            type: busType
         })
 
     });
