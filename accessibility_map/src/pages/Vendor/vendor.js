bf39405e5b9951a874077fd65f245471f1f38fce
Pei Yi Chiang <USER@lawn-128-61-68-189.lawn.gatech.edu>
2021-10-23 17:38:15 -0400

    initial vendor form set up

--- accessibility_map/src/pages/Vendor/vendor.js
+++ accessibility_map/src/pages/Vendor/vendor.js
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { TextField, FormControl, InputLabel, Select, MenuItem, Checkbox, Button, FormControlLabel, FormGroup, Container } from '@mui/material';
+import { TextField, FormControl, InputLabel, Select, MenuItem, Checkbox, Button, 
+    FormControlLabel, FormGroup, Grid } from '@mui/material';
+import { alpha } from '@mui/material/styles';
 import { servicesVersion } from 'typescript';
-import './vendor.css'
 
 
 const Vendor = () => {
     const [name, setName] = useState('');
-    const [addr, setAddr] = useState('');
+    const [street, setStreet] = useState('');
+    const [state, setState] = useState('');
+    let addr = street + ', ' + state;
     const [busType, setBusType] = useState('');
     const services = ['Allow Service Animal', 
                     'ASL accommodations',
@@ -22,8 +25,11 @@ const Vendor = () => {
     const updateName = (event) => {
         setName(event.target.value);
     }
-    const updateAddr = (event) => {
-        setAddr(event.target.value);
+    const updateStreet = (event) => {
+        setStreet(event.target.value);
+    }
+    const updateState = (event) => {
+        setState(event.target.value);
     }
     const updateDropdown = (event) => {
         setBusType(event.target.value);
@@ -32,36 +38,43 @@ const Vendor = () => {
         setChecked(prevState => prevState.map((item, idx) => idx === index ? !item : item));
     }
     const handleSubmit = () => {
-        alert(`User Created! Name: ${name} Address: ${addr} Type: ${busType} Services: ${checked}`);
+        alert(`Name: ${name} Address: ${addr} Type: ${busType} Services: ${checked}`);
     } 
    
     return (
         <form onSubmit={handleSubmit}>
-            <FormControl>
-                <Container className="container"/>
-                <TextField id="outlined-basic" label="Name" variant="outlined" onChange={updateName} value={name} required/>
-                <TextField id="outlined-basic" label="Address" variant="outlined" onChange={updateAddr} value={addr} required/>
-            {/* </FormControl>
-            <FormControl> */}
-                {/* <InputLabel>Business type</InputLabel> */}
-                <Select 
-                    value={busType} 
-                    displayEmpty
-                    onChange={updateDropdown}
-                >
-                    <MenuItem value={0}>Restaurant</MenuItem>
-                    <MenuItem value={1}>Park</MenuItem>
-                    <MenuItem value={2}>Public Transport</MenuItem>
-                    <MenuItem value={3}>Hair Dresser</MenuItem>
-                </Select>
-            {/* </FormControl>
-            <FormGroup> */}
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={6} md={8}>
+                    <TextField sx={{minWidth: '400px'}} id="outlined-basic" label="Name" variant="outlined" onChange={updateName} value={name} required/>
+                </Grid>
+                <Grid item xs={6} md={8}>
+                    <TextField sx={{minWidth: '400px'}} id="outlined-basic" label="Street" variant="outlined" onChange={updateStreet} value={street} required/>
+                    <TextField sx={{minWidth: '400px'}} id="outlined-basic" label="State" variant="outlined" onChange={updateState} value={state} required/>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Select 
+                        value={busType} 
+                        displayEmpty
+                        onChange={updateDropdown}
+                        sx={{minWidth: '400px'}}
+                    >
+                        <MenuItem value={0}>Restaurant</MenuItem>
+                        <MenuItem value={1}>Park</MenuItem>
+                        <MenuItem value={2}>Public Transport</MenuItem>
+                        <MenuItem value={3}>Hair Dresser</MenuItem>
+                    </Select>
+                </Grid>
                 {services.map((service, index) => (
-                    <FormControlLabel control={<Checkbox/>} onChange={setService} label={service} checked={checked[index]}/>
+                    <Grid item xs={12} md={8}>
+                        <FormControlLabel control={<Checkbox/>} onChange={setService} label={service} checked={checked[index]}/>
+                    </Grid>
                 ))}
-            
-                <Button type="submit" variant="outlined" onChange={handleSubmit}>Sign Up</Button>
-            </FormControl>
+                <Grid item xs={6} md={8}>
+                    <Button type="submit" variant="outlined" onChange={handleSubmit}>Sign Up</Button>
+                </Grid>
+            {/* </FormGroup>
+            </FormControl> */}
+            </Grid>
         </form>
     );
 };
